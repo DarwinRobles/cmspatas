ab347aef8495781f68460e713ba64e4dace8aa61
Darwin <darwinroblesreb@gmail.com>
2026-01-07 18:22:35 -0500

    Cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,25 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: [
+        'https://metidasdepata.com',
+        'https://www.metidasdepata.com',
+        'http://localhost:4200'
+      ],
+      headers: [
+        'Content-Type',
+        'Authorization',
+        'Origin',
+        'Accept'
+      ],
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      credentials: true
+    }
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
